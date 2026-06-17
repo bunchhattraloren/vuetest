dafdf3e732eed98e6be161d4b3cdc550256a4ead
Dev <dev@192.168.1.234>
2026-06-17 23:43:12 +0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold mb-4">Hello, Vite + Vue!</h1>
+        <p class="text-gray-700">This is a simple Vue application using Vite as the build tool.</p>
+    </div>  
+</template>
