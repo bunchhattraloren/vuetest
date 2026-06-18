9d2ba15a401904641dfdd8e79296f41684e556bc
Dev <dev@192.168.1.234>
2026-06-18 22:05:43 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,3 @@
 <template>
-  <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold mb-4">Hello, Vite + Vue!</h1>
-    <p class="text-gray-700">This is a simple Vue application using Vite as the build tool.</p>
-  </div>
+    <h1>About</h1>
 </template>
